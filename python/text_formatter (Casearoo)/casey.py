066421f1a72850663fb14a26082903aab7248701
Meliani Mehdi <mehdi.hatachi@gmail.com>
2025-03-05 10:25:16 +0100

    added new filters.

---
+++ python/text_formatter (Casearoo)/casey.py
@@ -0,0 +1,42 @@
+class Casey:
+    def sentenceCase(self, text):
+        latters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+        t = True
+        ren = ''
+        for l in text:
+            if l == ' ':
+                t = True
+                ren+=l
+            elif t and l in latters:
+                t = False
+                ren+=l.upper()
+            else:
+                ren+=l.lower()
+        return ren
+
+    def altCase(self, text):
+        pass
+
+    def upperCase(self, text):
+        return text.upper()
+
+    def lowerCase(self, text):
+        return text.lower()
+
+    def titleCase(self, text):
+        pass
+
+    def inverseCase(self, text):
+        pass
+
+    def reverseCase(self, text):
+        pass
+
+    def capitalizedCase(self, text):
+        pass
+
+
+
+if __name__ == "__main__":
+
+    pass
--- python/text_formatter (Casearoo)/text_ft_main.py
+++ python/text_formatter (Casearoo)/text_ft_main.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
+from casey import Casey as cs
 
 from PySide6.QtWidgets import QApplication, QWidget
 
@@ -14,6 +15,22 @@ class text_ft_main(QWidget):
         super().__init__(parent)
         self.ui = Ui_text_ft_main()
         self.ui.setupUi(self)
+
+        self.ui.sentenceCase.clicked.connect(lambda: self.useFunk(cs.sentenceCase))
+        self.ui.altCase.clicked.connect(lambda: self.useFunk(cs.altCase))
+        self.ui.upperCase.clicked.connect(lambda: self.useFunk(cs.upperCase))
+        self.ui.lowerCase.clicked.connect(lambda: self.useFunk(cs.lowerCase))
+        self.ui.titleCase.clicked.connect(lambda: self.useFunk(cs.titleCase))
+        self.ui.inverseCase.clicked.connect(lambda: self.useFunk(cs.inverseCase))
+        self.ui.reverseCase.clicked.connect(lambda: self.useFunk(cs.reverseCase))
+        self.ui.capitalizedCase.clicked.connect(lambda: self.useFunk(cs.capitalizedCase))
+
+    def useFunk(self, funk):
+        text = self.ui.main_text.toPlainText()
+        self.ui.formatter_holder.setDisabled(True)
+        self.ui.main_text.setText(funk(cs, text))
+        self.ui.formatter_holder.setDisabled(False)
+    
 
 
 if __name__ == "__main__":
